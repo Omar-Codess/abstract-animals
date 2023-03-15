4b5ff280be2f879a074fe1b83be823d1e0b44d18
Omar-Codess <omartaki8@gmail.com>
2023-03-15 15:21:29 +0100

    istanziati animali in main e testati metodi

---
+++ src/org/lessons/java/animals/Main.java
@@ -0,0 +1,30 @@
+package org.lessons.java.animals;
+
+public class Main {
+    public static void main (String[] args) {
+        Animale cane = new Cane("Tommy", "Mammifero");
+        Animale passerotto = new Passerotto("Charlie", "Uccello");
+        Animale aquila = new Aquila("Boh", "Uccello");
+        Animale delfino = new Delfino("Tommy", "Mammifero");
+
+        //CANE
+        cane.verso();
+        cane.mangia();
+        cane.dormi();
+
+        //PASSEROTTO
+        passerotto.verso();
+        passerotto.mangia();
+        passerotto.dormi();
+
+        //AQUILA
+        aquila.verso();
+        aquila.mangia();
+        aquila.dormi();
+
+        //DELFINO
+        delfino.verso();
+        delfino.mangia();
+        delfino.dormi();
+    }
+}
