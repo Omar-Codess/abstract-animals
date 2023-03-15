dcd0baa0bbe5f7ba33dcb6d56863eafc6cfd527b
Omar-Codess <omartaki8@gmail.com>
2023-03-15 14:36:45 +0100

    implementata classe Animale

--- src/org/lessons/java/animals/Animale.java
+++ src/org/lessons/java/animals/Animale.java
@@ -1,5 +1,30 @@
 package org.lessons.java.animals;
 
-public class Animale {
-    
+public abstract class Animale {
+    private String name;
+    private String type;
+
+
+    public Animale(String name, String type) {
+        this.name = name;
+        this.type = type;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public abstract void dormi();
 }
